7b05b4774620696be1a7cca2e5c74c3a384150ff
Erik Nero Mounier <524102@student.glu.nl>
2018-01-24 14:18:38 +0100

    Update Holy Shrine
    1. added the dark enemies

--- BulletHell/Assets/scripts/Spawn.cs
+++ BulletHell/Assets/scripts/Spawn.cs
@@ -220,6 +220,66 @@ public class Spawn : MonoBehaviour {
             else if (Theme_amount == 2) //shrine
             {
 
+                Beach.transform.position = Offscreen_Storage;
+                Shrine.transform.position = loc;
+                if (wave_amount >= 0 && wave_start == true && wave_amount <= 1)
+                {
+                    pos.y = 7;
+                    pos.x = -8;
+                    store_Enemy[0] = Instantiate(D_enemy_lv1, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount == 1 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = -4;
+                    store_Enemy[1] = Instantiate(D_enemy_lv2, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 2 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 4;
+                    store_Enemy[2] = Instantiate(D_enemy_lv3, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 3 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 8;
+                    store_Enemy[3] = Instantiate(D_minion, pos, transform.rotation) as GameObject;
+                }
+                wave_start = false;
+                if (Time_of_wave >= 20f)
+                {
+                    if (store_Enemy[0] != null)
+                    {
+                        Destroy(store_Enemy[0].gameObject);
+                    }
+                    if (store_Enemy[1] != null)
+                    {
+                        Destroy(store_Enemy[1].gameObject);
+                    }
+                    if (store_Enemy[2] != null)
+                    {
+                        Destroy(store_Enemy[2].gameObject);
+                    }
+                    if (store_Enemy[3] != null)
+                    {
+                        Destroy(store_Enemy[3].gameObject);
+                    }
+                    //if (player != null)
+                    //{
+                    //    Destroy(player);
+                    //    player = Instantiate(player, pos, transform.rotation);
+                    //}
+
+
+                    wave_start = true;
+                    wave_amount += 1;
+                    Time_of_wave = 0;
+                }
+                if (wave_amount >= 4)
+                {
+                    Theme_amount += 1;
+                }
 
 
 
