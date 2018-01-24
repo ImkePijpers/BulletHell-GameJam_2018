e7b67a37dc55486969681e47a22f557619881ca7
Erik Nero Mounier <524102@student.glu.nl>
2018-01-24 12:34:35 +0100

    major fixes

--- BulletHell/Assets/scripts/Spawn.cs
+++ BulletHell/Assets/scripts/Spawn.cs
@@ -53,6 +53,7 @@ public class Spawn : MonoBehaviour {
     Vector3 Offscreen_Storage = new Vector3(90,90,90);
     Random Randomizer = new Random();
 
+    float preparing_time;
     float Time_of_wave;
     int amount_of_enemies;
     int wave_amount;
@@ -78,143 +79,151 @@ public class Spawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Time_of_wave += Time.deltaTime;
+        preparing_time += Time.deltaTime;
 
-        if(Theme_amount == 0) //grasslands
-        {
-            grasslands.transform.position = loc;
-            if(wave_amount >= 0 && wave_start == true && wave_amount <= 1)
-            {
-                pos.y = 7;
-                pos.x = -8;
-                store_Enemy[0] = Instantiate(enemy_lv1, pos, transform.rotation) as GameObject;
-            }
-            if (wave_amount == 1 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = -4;
-                store_Enemy[1] = Instantiate(enemy_lv2, pos, transform.rotation) as GameObject;
-            }
-            if (wave_amount >= 2 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = 4;
-                store_Enemy[2] = Instantiate(enemy_lv3, pos, transform.rotation)as GameObject;
-            }
-            if (wave_amount >= 3 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = 8;
-                store_Enemy[3] = Instantiate(minion, pos, transform.rotation)as GameObject;
-            }
-            wave_start = false;
-            if(Time_of_wave >= 20f)
-            {
-                if (store_Enemy[0] != null)
-                {
-                    Destroy(store_Enemy[0].gameObject);
-                }
-                if (store_Enemy[1] != null)
-                {
-                    Destroy(store_Enemy[1].gameObject);
-                }
-                if (store_Enemy[2] != null)
-                {
-                    Destroy(store_Enemy[2].gameObject);
-                }
-                if (store_Enemy[3] != null)
-                {
-                    Destroy(store_Enemy[3].gameObject);
-                }
-                //if (player != null)
-                //{
-                //    Destroy(player);
-                //    player = Instantiate(player, pos, transform.rotation);
-                //}
-
-
-                wave_start = true;
-                wave_amount += 1;
-                Time_of_wave = 0;
-            }
-            if (wave_amount >= 4)
-            {
-                Theme_amount += 1;
-            }
-
-        }
-        else if(Theme_amount == 1) //beach
-        {
-            grasslands.transform.position = Offscreen_Storage;
-            Beach.transform.position = loc;
-            if (wave_amount >= 0 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = -8;
-                store_Enemy[0] = Instantiate(W_enemy_lv1, pos, transform.rotation) as GameObject;
-            }
-            if (wave_amount >= 1 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = -4;
-                store_Enemy[1] = Instantiate(W_enemy_lv2, pos, transform.rotation) as GameObject;
-            }
-            if (wave_amount >= 2 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = 4;
-                store_Enemy[2] = Instantiate(W_enemy_lv3, pos, transform.rotation) as GameObject;
-            }
-            if (wave_amount >= 3 && wave_start == true)
-            {
-                pos.y = 7;
-                pos.x = 8;
-                store_Enemy[3] = Instantiate(W_minion, pos, transform.rotation) as GameObject;
-            }
-            wave_start = false;
-            if (Time_of_wave >= 20f)
-            {
-                if (store_Enemy[0] != null)
-                {
-                    Destroy(store_Enemy[0].gameObject);
-                }
-                if (store_Enemy[1] != null)
-                {
-                    Destroy(store_Enemy[1].gameObject);
-                }
-                if (store_Enemy[2] != null)
-                {
-                    Destroy(store_Enemy[2].gameObject);
-                }
-                if (store_Enemy[3] != null)
-                {
-                    Destroy(store_Enemy[3].gameObject);
-                }
-                //if (player != null)
-                //{
-                //    Destroy(player);
-                //    player = Instantiate(player, pos, transform.rotation);
-                //}
-
-
-                wave_start = true;
-                wave_amount += 1;
-                Time_of_wave = 0;
-            }
-            if (wave_amount >= 4)
-            {
-                Theme_amount += 1;
-            }
-
-
-
-        }
-        else if (Theme_amount == 2) //shrine
+        if (preparing_time >= 0.5f)
         {
 
+            Time_of_wave += Time.deltaTime;
+
+            if (Theme_amount == 0) //grasslands
+            {
+                grasslands.transform.position = loc;
+                if (wave_amount >= 0 && wave_start == true && wave_amount <= 1)
+                {
+                    pos.y = 7;
+                    pos.x = -8;
+                    store_Enemy[0] = Instantiate(enemy_lv1, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount == 1 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = -4;
+                    store_Enemy[1] = Instantiate(enemy_lv2, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 2 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 4;
+                    store_Enemy[2] = Instantiate(enemy_lv3, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 3 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 8;
+                    store_Enemy[3] = Instantiate(minion, pos, transform.rotation) as GameObject;
+                }
+                wave_start = false;
+                if (Time_of_wave >= 20f)
+                {
+                    if (store_Enemy[0] != null)
+                    {
+                        Destroy(store_Enemy[0].gameObject);
+                    }
+                    if (store_Enemy[1] != null)
+                    {
+                        Destroy(store_Enemy[1].gameObject);
+                    }
+                    if (store_Enemy[2] != null)
+                    {
+                        Destroy(store_Enemy[2].gameObject);
+                    }
+                    if (store_Enemy[3] != null)
+                    {
+                        Destroy(store_Enemy[3].gameObject);
+                    }
+                    //if (player != null)
+                    //{
+                    //    Destroy(player);
+                    //    player = Instantiate(player, pos, transform.rotation);
+                    //}
+
+
+                    wave_start = true;
+                    wave_amount += 1;
+                    Time_of_wave = 0;
+                }
+                if (wave_amount >= 4)
+                {
+                    Theme_amount += 1;
+                    wave_amount = 0;
+                }
+
+            }
+            else if (Theme_amount == 1) //beach
+            {
+                
+                grasslands.transform.position = Offscreen_Storage;
+                Beach.transform.position = loc;
+                if (wave_amount >= 0 && wave_start == true && wave_amount <= 1)
+                {
+                    pos.y = 7;
+                    pos.x = -8;
+                    store_Enemy[0] = Instantiate(W_enemy_lv1, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount == 1 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = -4;
+                    store_Enemy[1] = Instantiate(W_enemy_lv2, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 2 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 4;
+                    store_Enemy[2] = Instantiate(W_enemy_lv3, pos, transform.rotation) as GameObject;
+                }
+                if (wave_amount >= 3 && wave_start == true)
+                {
+                    pos.y = 7;
+                    pos.x = 8;
+                    store_Enemy[3] = Instantiate(W_minion, pos, transform.rotation) as GameObject;
+                }
+                wave_start = false;
+                if (Time_of_wave >= 20f)
+                {
+                    if (store_Enemy[0] != null)
+                    {
+                        Destroy(store_Enemy[0].gameObject);
+                    }
+                    if (store_Enemy[1] != null)
+                    {
+                        Destroy(store_Enemy[1].gameObject);
+                    }
+                    if (store_Enemy[2] != null)
+                    {
+                        Destroy(store_Enemy[2].gameObject);
+                    }
+                    if (store_Enemy[3] != null)
+                    {
+                        Destroy(store_Enemy[3].gameObject);
+                    }
+                    //if (player != null)
+                    //{
+                    //    Destroy(player);
+                    //    player = Instantiate(player, pos, transform.rotation);
+                    //}
+
+
+                    wave_start = true;
+                    wave_amount += 1;
+                    Time_of_wave = 0;
+                }
+                if (wave_amount >= 4)
+                {
+                    Theme_amount += 1;
+                }
 
 
 
+            }
+            else if (Theme_amount == 2) //shrine
+            {
+
+
+
+
+            }
         }
 
 
