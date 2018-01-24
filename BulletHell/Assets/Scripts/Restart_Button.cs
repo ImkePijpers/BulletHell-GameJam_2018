60e38b1308160aa61cdbe2fafbbbde694f3340c0
Imke <Imke.P@hotmail.com>
2018-01-24 12:21:13 +0100

    Wohoo Buttons fucking work
    - start button
    -quit button
    -restart button
    -back to main menu button
    
    betere manier ervoor gevonden

---
+++ BulletHell/Assets/Scripts/Restart_Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart_Button : MonoBehaviour {
+
+    [SerializeField]
+    private Button ReStart_Button;
+
+
+
+
+
+    void Update()
+    {
+        ReStart_Button.onClick.AddListener(Load_scene);
+
+
+    }
+
+    public void Load_scene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
